134998f25a7ec6efe70b1bce50bff0d56905eac0
Hayden Larson <hlarson398@gmail.com>
2019-07-15 18:48:28 -0500

    built the counters in java, randomized the computer's selection, and tracking wins & loses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 console.log("HI!");
 
-var computerChoices = ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
+var computerChoices = ["q"] //, "w", "e", "r", "t", "y", "u", "i", "o", "p", "a", "s", "d", "f", "g", "h", "j", "k", "l", "z", "x", "c", "v", "b", "n", "m"];
 
 console.log(computerChoices.length);
 
@@ -14,15 +14,15 @@ console.log(losses);
 console.log(guessesLeft);
 console.log(guessed);
 
-var winColumn = document.getElementById("wins");
-var lossColumn = document.getElementById("losses");
-var guessesColumn = document.getElementById("guesses-left");
-var guessedColumn = document.getElementById("guessed");
+var winText = document.getElementById("wins");
+var lossText = document.getElementById("losses");
+var guessesText = document.getElementById("guesses-left");
+var lettersGuessedText = document.getElementById("guessed");
 
-console.log(winColumn);
-console.log(lossColumn);
-console.log(guessesColumn);
-console.log(guessedColumn);
+console.log(winText);
+console.log(lossText);
+console.log(guessesText);
+console.log(lettersGuessedText);
 
 document.onkeyup = function (event) {
 
@@ -31,14 +31,32 @@ document.onkeyup = function (event) {
     console.log(userGuess);
     console.log(computerGuess);
 
-    //if (userGuess === computerGuess) {
-    //    wins++;
+    if (userGuess === computerGuess) {
+        wins++;
+        console.log(wins);
+        //lettersGuessedText.textContent = "";
+        console.log(lettersGuessedText);
+        console.log(guessesLeft);
+        guessesLeft = 9;
+        console.log(guessesLeft);
 
-//    } else if (userGuess !== computerGuess) {
-  //      guessedColumn.textContent = [userGuess(event.key)];
-    //}
+        //next step needs to reset the "Guesses Left" and clear the "Your Guesses so far"
 
+    } else if(userGuess !== computerGuess){
+        guessesLeft--;
+        console.log(guessesLeft);
+        //var i = lettersGuessedText;
+        //for (i = 0; i < guessed.length; i--);
+        //guessedColumn.textContent = [i];
+        if (guessesLeft === 0){
+            losses++;
+            guessesLeft = 9;
 
-    //winColumn.textContent = wins;
-}
+        };
+        console.log(losses);
+    }
 
+    // winColumn.textContent = wins;
+    //lettersGuessedText.textContent = "You chose: " + userGuess;
+    //winText.textContent = wins;
+};
